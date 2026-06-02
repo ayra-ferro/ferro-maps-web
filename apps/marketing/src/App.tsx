d4070c4d30c86b1ab23272c22d48760e2f7c3ff3
AyraAhmed <ayraahmed04@gmail.com>
2026-06-02 17:18:06 +0100

    FM-WEB-018: wire all sections into App.tsx with correct order and section layout

--- apps/marketing/src/App.tsx
+++ apps/marketing/src/App.tsx
@@ -2,7 +2,9 @@ import Header from './Header'
 import Hero from './Hero'
 import WhyFerroMaps from './WhyFerroMaps'
 import HowItWorks from './HowItWorks'
+import Testimonials from './Testimonials'
 import AppScreenshots from './AppScreenshots'
+import Contact from './Contact'
 import DownloadBanner from './DownloadBanner'
 import Footer from './Footer'
 
@@ -14,11 +16,14 @@ export default function App() {
 
       <main>
         <WhyFerroMaps />
-        <HowItWorks />
         <AppScreenshots />
+        <HowItWorks />
+        <Testimonials />
+        
       </main>
 
       <DownloadBanner />
+      <Contact />
       <Footer />
     </div>
   )
