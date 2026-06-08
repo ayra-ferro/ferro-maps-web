82249b183587289b2ffd09b384fdb7f80e4374b4
AyraAhmed <ayraahmed04@gmail.com>
2026-06-08 13:02:46 +0100

    FM-WEB-029: add hash-based scroll on homepage after React Router navigation
    - useEffect watches location.hash and scrolls to section after 100ms delay

--- apps/marketing/src/App.tsx
+++ apps/marketing/src/App.tsx
@@ -1,3 +1,6 @@
+import { useEffect } from 'react'
+import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { HelmetProvider } from 'react-helmet-async'
 import Header from './Header'
 import Hero from './Hero'
 import WhyFerroMaps from './WhyFerroMaps'
@@ -7,10 +10,25 @@ import AppScreenshots from './AppScreenshots'
 import Contact from './Contact'
 import DownloadBanner from './DownloadBanner'
 import Footer from './Footer'
+import SEOHead from './SEOHead'
+import PrivacyPolicy from './pages/PrivacyPolicy'
 
-export default function App() {
+function Home() {
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.hash) {
+      const id = location.hash.slice(1)
+      const timer = setTimeout(() => {
+        document.getElementById(id)?.scrollIntoView({ behavior: 'smooth' })
+      }, 100)
+      return () => clearTimeout(timer)
+    }
+  }, [location.hash])
+
   return (
     <div className="min-h-screen bg-white text-text-primary font-geist">
+      <SEOHead />
       <Header />
       <Hero />
 
@@ -19,12 +37,24 @@ export default function App() {
         <AppScreenshots />
         <HowItWorks />
         <Testimonials />
-        
       </main>
 
       <DownloadBanner />
       <Contact />
       <Footer />
     </div>
+  )
+}
+
+export default function App() {
+  return (
+    <HelmetProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+        </Routes>
+      </BrowserRouter>
+    </HelmetProvider>
   )
 }
